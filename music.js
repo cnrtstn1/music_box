06fcb0fb4e06f514b81325af67d1fdd4abd3582f
Caner Taştan <cnrtstn1@gmail.com>
2025-01-17 13:08:48 +0300

    add music box

---
+++ music.js
@@ -0,0 +1,89 @@
+const musicList = document.getElementById("musicList");
+const songs = [];
+let currentAudio = null;
+let isPlaying = false;
+
+function showMusicSection() {
+    document.getElementById("showButton").style.display = "none";
+    document.getElementById("musicDiv").style.display = "block";
+    loadMusicList();
+}
+
+function loadMusicList() {
+    musicList.innerHTML = "";
+    songs.forEach((song, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${song.name}`;
+
+        const playPauseButton = document.createElement("button");
+        playPauseButton.innerText = "Play";
+        playPauseButton.onclick = () => playPause(index, playPauseButton);
+
+        li.appendChild(playPauseButton);
+        musicList.appendChild(li);
+    });
+}
+
+
+const fileInput = document.getElementById("fileInput");
+fileInput.addEventListener('change', handleFile);
+
+function handleFile(event) {
+    const file = event.target.files[0];
+
+    if (!file || (!file.name.endsWith('.mp3'))) {
+        return alert("HATALI DOSYA");
+    }
+
+    const songName = file.name;
+    const song = {
+        name: songName,
+        url: URL.createObjectURL(file)
+    };
+
+    songs.unshift(song);
+
+    loadMusicList();
+    fileInput.value = null
+}
+
+function playPause(index, button) {
+    if (currentAudio && currentAudio !== songs[index].audio) {
+        currentAudio.pause();
+        isPlaying = false;
+    }
+
+    if (!songs[index].audio) {
+        songs[index].audio = new Audio(songs[index].url);
+    }
+
+    currentAudio = songs[index].audio;
+
+    if (isPlaying) {
+        currentAudio.pause();
+        button.innerText = "Play";
+    } else {
+        currentAudio.play();
+        button.innerText = "Pause";
+    }
+
+    isPlaying = !isPlaying;
+}
+
+
+// ad,
+//     soyad,
+//     telefon,
+//     profil foto yuvarlak.
+ 
+// Form olacak ve submit edeceksin.
+
+// Tabloda bilgiler sırasıyla gösterilecek. 
+
+
+// telefonun input  içine + 90 5384444444
+
+// telefonu yanında ara tuşu ve whatsupp arama yapabilcek.
+
+
+// sayfa yenilenince kayıtlı tüm datalar ekranda kalmaya devam edecek.localstorage a kaydet.
